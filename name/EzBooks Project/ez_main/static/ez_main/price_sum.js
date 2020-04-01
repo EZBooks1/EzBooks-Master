e3e23f5be7c4f85ea708b98792d3ead8936c7a67
joshdevera <55072501+joshdevera@users.noreply.github.com>
2020-04-01 10:53:09 -0400

    Add files via upload
    Contact & Info page updated and Random class generator button name change

---
+++ name/EzBooks Project/ez_main/static/ez_main/price_sum.js
@@ -0,0 +1,73 @@
+function calculate_pricing()
+{
+   var buyButton  = document.getElementsByClassName('buy');  // Array of all the buy buttons
+   var rentButton = document.getElementsByClassName('rent'); // Array of all the rent buttons
+   var newButton  = document.getElementsByClassName('new');  // Array of all the new buttons
+   var usedButton = document.getElementsByClassName('used'); // Array of all the used buttons
+
+   var index;
+   var length = buyButton.length;
+   var text;
+   var matches;
+   var priceTotal = 0;
+
+   var newPrice;  // Array of only new prices
+   var usedPrice; // Array of only used prices
+
+   for(index = 0; index < length; index++)
+   {
+      if(buyButton[index].checked)
+      {
+         newPrice = document.getElementsByClassName('newBuy');
+         usedPrice = document.getElementsByClassName('usedBuy');  
+      }
+      else if(rentButton[index].checked)
+      {
+         newPrice = document.getElementsByClassName('newRent');
+         usedPrice = document.getElementsByClassName('usedRent'); 
+      }
+
+      // Get the correct price (new or used)
+      if(newButton[index].checked)
+      {
+         text = newPrice[index].innerText;
+      }
+      else if(usedButton[index].checked)
+      {
+         text = usedPrice[index].innerText;
+      }
+
+      // Get the price as a number
+      matches = text.match(/(\d+)/);
+
+      if(matches)
+      {
+         var num = parseInt(matches[0]);
+         priceTotal = priceTotal + num;
+      }
+   }
+
+   var taxAmount       = (priceTotal * .06);
+   var fixedTaxAmount  = taxAmount.toFixed(2);
+   var grandTotal      = priceTotal + taxAmount;
+   var fixedGrandTotal = grandTotal.toFixed(2);
+
+   document.getElementById("total_price").innerHTML = "$" + priceTotal;
+   document.getElementById("tax_amount").innerHTML  = "$" + fixedTaxAmount;
+   document.getElementById("grand_total").innerHTML = "$" + fixedGrandTotal;
+
+   document.getElementById("total").value = fixedGrandTotal;
+   return;
+
+}
+
+if(window.addEventListener)
+{
+   window.addEventListener("load", calculate_pricing, false);
+}
+else if (window.attachEvent)
+{
+   window.attachEvent("onload", calculate_pricing);
+}
+
+window.addEventListener("click", calculate_pricing, false);
---
+++ name/EzBooks Project/ez_main/urls.py
@@ -0,0 +1,38 @@
+###############################################################################
+#                      Url Patterns for the ez_main app                       #
+###############################################################################
+
+from django.urls import path
+
+from . import views
+
+# Url patterns for the ez_main app
+app_name = 'ez_main'
+urlpatterns = [
+   # Home page path
+   path('', views.home_page, name='home_page'),
+
+   # About page path
+   path('about/', views.about_page, name='about_page'),
+
+   # Class page path
+   path('class/', views.class_page, name='class_page'),
+
+   # Book page path
+   path('books/', views.books_page, name='books_page'),
+
+   # Checkout summary page path
+   path('summary/', views.checkout_summary, name='checkout_summary'),
+
+   # Checkout page path
+   path('checkout/', views.checkout_page, name='checkout_page'),
+
+   # Checkout submission page
+   path('confirmation/', views.confirmation_page, name='confirmation_page'),
+
+   # Guest login page
+   path('guests/', views.guest_login, name="guest_login"),
+
+   # Contact Info & Help page path
+   path('contactinfo/', views.contactinfo_page, name="contactinfo_page")
+]
